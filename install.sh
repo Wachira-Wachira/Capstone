ca0546faa93d8e529553e8a8f00868ab6245a426
Wachira-Wachira <92554974+Wachira-Wachira@users.noreply.github.com>
2022-02-16 17:16:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,5 +25,9 @@ python3 -m venv my project-env
 sudo source env/bin/activate
 python3 -m pip install requests
 
+#creating migrations
+sudo python3 /users/mob_app_usr/manage.py makemigrations
+sudo python3 /users/mob_app_usr/manage.py migrate
+
 
 
