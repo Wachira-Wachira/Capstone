d5cc1490fb4fad569436b542cda2ea30855de933
Wachira-Wachira <92554974+Wachira-Wachira@users.noreply.github.com>
2022-02-16 13:17:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 sudo su
 passwd -d mob_app_usr
 su - mob_app_usr
+
+#clone the repository to home directory
 git clone https://github.com/Wachira-Wachira/Capstone.git
+
+#set environment variables
+echo 'export DBNAME=mobalytics >> ~/.bashrc
+echo 'export DBUSER=mob_db_user >> ~/.bashrc
+echo 'export DBPASS=mob_db_pass >> ~/.bashrc
+echo 'export DBHOST=localhost >> ~/.bashrc
+echo 'export DBPORT=5432 >> ~/.bashrc
+#to execute in the current session
+.~/.bashrc
 exit
+
 
