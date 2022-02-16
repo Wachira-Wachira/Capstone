5f8187cb5d3b7cd43278cf690d5a182c21294711
Wachira-Wachira <92554974+Wachira-Wachira@users.noreply.github.com>
2022-02-16 13:37:39 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ sudo apt-get -y install postgresql
 sudo service postgresql start
 sudo -u postgres psql -c"create user mob_db_user with encrypted password 'mob_db_pass';"
 sudo service postgresql restart
-sudo useradd mob_app_usr
-sudo mkhomedir_helper mob_app_usr
 sudo useradd -m mob_app_usr
 
