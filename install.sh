4698ed0d4e2a5dc6426242e3e64463e6ae9fa263
Inyanji-bw <99354150+Inyanji-bw@users.noreply.github.com>
2022-02-16 17:11:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,14 @@ echo 'export DBPORT=5432 >> ~/.bashrc
 .~/.bashrc
 exit
 
+#Create a virtual environment and installing application packages
+sudo apt update
+sudo apt install python3-pip
+pip install virtualenv
+sudo apt install python3.8-venv
+python3 -m venv my project-env
+sudo source env/bin/activate
+python3 -m pip install requests
+
+
 
