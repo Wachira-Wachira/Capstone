15fc04d533f23625a1c77c65b8bfb4920595ca86
Wachira-Wachira <92554974+Wachira-Wachira@users.noreply.github.com>
2022-02-16 18:49:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ echo 'export DBPORT=5432 >> ~/.bashrc
 
 #Create a virtual environment and installing application packages
 sudo apt update
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 pip install virtualenv
-sudo apt install python3.8-venv
+sudo apt -y install python3.8-venv
 python3 -m venv my project-env
 sudo source env/bin/activate
 python3 -m pip install requests
