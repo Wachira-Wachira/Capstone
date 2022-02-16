ffa91f5a3f430afde8963c016aeda87f816cb8cc
Antoinette Akinyi <61618082+Kisha-net@users.noreply.github.com>
2022-02-16 15:33:50 +0300

    updating setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ sudo -u postgres psql -c"create user mob_db_user with encrypted password 'mob_db
 sudo service postgresql restart
 sudo useradd -m mob_app_usr
 
+sudo apt update
+sudo apt install python3-pip
+pip install virtualenv
+sudo apt install python3.8-venv
+python3 -m venv my project-env 
+sudo source env/bin/activate 
+python3 -m pip install requests 
+sudo -u postgres psql -c"create database mobalytics owner mob_db_user;"
